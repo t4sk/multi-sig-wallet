184e4a0c5b7c68c06867f33c29fc9787da5be9e1
t4sk <tsk.nakamura@gmail.com>
2020-04-21 13:16:52 +0900

    footer

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -5,15 +5,15 @@ import styles from "./Footer.module.css";
 
 interface Props {}
 
-const Footer: React.FC<Props> = ({}) => {
+const Footer: React.FC<Props> = () => {
   return (
     <div className={styles.component}>
-      <img src={github} className={styles.image} />
+      <img src={github} className={styles.image} alt="github" />
       <a href="https://github.com/t4sk/multi-sig-wallet" target="__blank">
         Code
       </a>
       <div className={styles.bar}>|</div>
-      <img src={scp} className={styles.image} />
+      <img src={scp} className={styles.image} alt="smart contract programmer" />
       <a href="https://smartcontractprogrammer.com" target="__blank">
         Smart Contract Programmer
       </a>
